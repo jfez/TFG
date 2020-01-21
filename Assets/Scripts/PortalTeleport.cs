e01d9e43843e0fc28904ace829478cd3cd7ba36c
Jorge Fernández <44475543+ixa10@users.noreply.github.com>
2020-01-21 23:45:45 +0100

    Portals

---
+++ Assets/Scripts/PortalTeleport.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTeleport : MonoBehaviour
+{
+    //public Transform player;
+    public CharacterController player;
+	public Transform reciever;
+
+	private bool playerIsOverlapping = false;
+
+	// Update is called once per frame
+	void Update () {
+		if (playerIsOverlapping)
+		{
+			Vector3 portalToPlayer = player.transform.position - transform.position;
+			float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
+
+			// If this is true: The player has moved across the portal
+			if (dotProduct < 0f)
+			{
+				// Teleport him!
+				float rotationDiff = -Quaternion.Angle(transform.rotation, reciever.rotation);
+				rotationDiff += 180;
+				player.transform.Rotate(Vector3.up, rotationDiff);
+
+				Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
+				player.enabled =false;
+                player.transform.position = reciever.position + positionOffset;
+                player.enabled =true;
+
+				playerIsOverlapping = false;
+			}
+		}
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			playerIsOverlapping = true;
+		}
+	}
+
+	void OnTriggerExit (Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			playerIsOverlapping = false;
+		}
+	}
+}
