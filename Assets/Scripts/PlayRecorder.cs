4ad4dd5e5a2aa4f50decc272739e68faad1da5dd
Jorge Fernández <44475543+ixa10@users.noreply.github.com>
2020-01-22 20:54:29 +0100

    AudioRecorder

---
+++ Assets/Scripts/PlayRecorder.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayRecorder : MonoBehaviour
+{
+    public Transform Eyes;
+
+    private AudioSource recorder;
+    private float playDistance;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        recorder = GetComponent<AudioSource>();
+        playDistance = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(Eyes.position, Eyes.forward, out hit, Mathf.Infinity))
+            {
+                if (hit.transform.tag == "AudioRecorder")
+                {
+                    //If we want to limitate the distance to play the recorder
+                    if (Vector3.Distance(Eyes.position, transform.position) < playDistance)
+                    {
+                        recorder.Play();
+                    }
+                    
+                    
+                }
+            }
+        }
+                
+    }
+}
