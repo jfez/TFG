0ad6dd83e3a5674a657de384795fb27af6c50f5a
CapDePinya <43250158+CapDePinya@users.noreply.github.com>
2020-02-27 17:49:02 +0100

    Toon Shader, Cerezos  y Sistema de Particulas

---
+++ Assets/Shaders/Toon Shader/ToonShaderLightSettings.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ToonShaderLightSettings : MonoBehaviour
+{
+    new private Light light;
+
+    void OnEnable()
+    {
+        light = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Shader.SetGlobalVector("_ToonLightDirection", -transform.forward);
+        Shader.SetGlobalColor("_ToonLightColor", light.color);
+    }
+}
