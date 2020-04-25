ea14766aec5209cc53d38eb724373ccb1cf26b4c
Jorge Fernández <44475543+ixa10@users.noreply.github.com>
2020-04-25 13:25:52 +0200

    loop manager, maze camera, jail animation and apple script

---
+++ Assets/Scripts/Gameplay/Loop Manager/LoopManagerGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoopManagerGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.N) && Input.GetKey(KeyCode.M))
+        {
+            PlayerPrefs.SetInt("Loop", 1);
+            Debug.Log("LOOP BROKEN");
+            SceneManager.LoadScene("Init");
+        }
+    }
+}
---
+++ Assets/Scripts/Init/LoopManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoopManager : MonoBehaviour
+{
+    public Button playButton;
+    private AudioSource audioSource;
+    private float timePlaying;
+    private bool timeRunning;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        timePlaying = 0f;
+        timeRunning = false;
+        
+        if (!PlayerPrefs.HasKey("Loop"))
+        {
+            PlayerPrefs.SetInt("Loop", 0);
+        }
+
+        //Loop is broken
+        if (PlayerPrefs.GetInt("Loop") != 0)
+        {
+            playButton.interactable = false;
+            audioSource.Play();
+            timeRunning = true;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeRunning)
+        {
+            if (timePlaying <= audioSource.clip.length)
+            {
+                timePlaying += Time.deltaTime;
+            }
+
+            else
+            {
+                Debug.Log("QUIT");
+                Application.Quit();
+            }
+            
+        }
+        
+        if (Input.GetKey(KeyCode.F1) && Input.GetKey(KeyCode.F2))
+        {
+            if (PlayerPrefs.GetInt("Loop") != 0)
+            {
+                playButton.interactable = true;
+                audioSource.Stop();
+                timeRunning = false;
+                PlayerPrefs.SetInt("Loop", 0);
+            }
+        }
+    }
+}
